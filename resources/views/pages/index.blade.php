6763bb0e70a32ef83a085454d863c5ff5d0daa3b
jcsegismundo <31530644+jcsegismundo@users.noreply.github.com>
2017-12-03 23:50:43 +0800

    Changed look

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,44 +5,68 @@
 
 <!-- First Section -->
 
-    <div class="jumbotron text-center" style="margin-bottom:0;">
+    <div class="jumbotron text-center" style="background-color:#00aae7;margin-bottom:0;">
         <div class="container">
-            <h1>Scholarship App</h1>
-            <img src="{{ URL::to('/') }}/images/bird.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
-            <h3>Find scholarships that matches you with app_name</h3>
-            <a href="/match" class="btn btn-primary"><h3>Start matching now!</h3></a>
+            <h1 style="color:white; font-weight:bold">
+                Comprehensive Application for Retrieving List of Scholarships
+            </h1>
+            <h2 style="color:white;">
+                Developing a Real Opportunity for Your Acads
+            </h2>
+            <img src="{{ URL::to('/') }}/images/daroya.png" class="img-responsive img-circle margin" style="display:inline" alt="daroya" width="250" height="250">
+            <h4 style="color:white;">
+                Find scholarships that matches you with App_name
+            </h4>
+            <a href="/match" class="btn btn-primary" style="background-color:#f27749;">
+                <h3>
+                    Start matching now!
+                </h3>
+            </a>
         </div>
     </div>
 
 <!-- Second Container -->
-    <div class="jumbotron text-center" style="background-color:powderblue; margin-bottom:0;">
-        <h2>What Am I?</h2>
-        <h3> Currently, App_name supports an abundant list of scholarships from universities in metro manila. Expect more partner schools in the future  </h3>
-        <a href="/colleges" class="btn btn-default btn-lg">
-            <span class="glyphicon glyphicon-search"></span> See Current Universities
+    <div class="jumbotron text-center" style="background-color:white; margin-bottom:0;">
+        <h2>
+            What Am I?
+        </h2>
+        <h3>
+            Currently, App_name supports an abundant list of scholarships from universities in metro manila. Expect more partner schools in the future  
+        </h3>
+        <a href="/colleges" class="btn btn-default btn-lg" style="background-color:#00aae7; color:white;">
+           <!-- <span class="glyphicon glyphicon-search"></span> See Current Universities -->
+            <img src="{{ URL::to('/') }}/images/whiteglass.png" class="img-responsive" style="display:inline" alt="whiteglass" width="20" height="20"> 
+                See Current Universities
         </a>
     </div>
 
 <!-- Third Container -->
-    <div class="jumbotron text-center" style="background-color:Springgreen; margin-bottom:0;">
-        <h2>See all the $$$ scholarships in our database.View them by category depending on your needs</h2>
-        <a href="/scholarships" class="btn btn-default btn-lg">
-            <span class="glyphicon glyphicon-search"></span> Browse scholarships
+    <div class="jumbotron text-center" style="background-color:white; margin-bottom:0; ">
+        <h2>
+            See all the $$$ scholarships in our database.View them by category depending on your needs
+        </h2>
+        <a href="/scholarships" class="btn btn-default btn-lg" style="background-color:#00aae7; color:white">
+            <!-- <span class="glyphicon glyphicon-search"></span> Browse scholarships -->
+            <img src="{{ URL::to('/') }}/images/whiteglass.png" class="img-responsive" style="display:inline" alt="whiteglass" width="20" height="20"> 
+                Browse Scholarships
         </a>
     </div>
 
 <!-- Fourth Container -->
-    <div class="jumbotron text-center" style="background-color:Lightyellow; margin-bottom:0;">
-        <h2>-	Let app_name take care of finding scholarships that you qualify into</h2>
-        <a href="/match" class="btn btn-default btn-lg">
-            <span class="glyphicon glyphicon-search"></span> Start matching 
+    <div class="jumbotron text-center" style="background-color:#157fc3; margin-bottom:0; color:white">
+        <h2>
+            Let app_name take care of finding scholarships that you qualify into
+        </h2>
+        <a href="/match" class="btn btn-default btn-lg" style="background-color:#f27749; color:white">
+            <!-- <span class="glyphicon glyphicon-search"></span> Start matching -->
+            <img src="{{ URL::to('/') }}/images/whiteglass.png" class="img-responsive" style="display:inline" alt="glass" width="20" height="20"> 
+                Start Matching!
         </a>
     </div>
 
 <!-- Footer -->
-    <footer class="jumbotron text-center" style ="background-color:gray;">
-        <p>Made by <a href="/about"> Me</a></p> </br>
-        <p>See our partner  <a href="/companies"> companies</a></p> </br>
+    <footer class="jumbotron text-center" style ="background-color:#4a4a4a; color:white">
+        <p>Made by <a href="/about"> Me</a></p> 
     </footer>
 
 @endsection
